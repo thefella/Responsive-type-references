604b7f123efdd6c606a0d511e4629befda6486c6
Conor MacNeill <github@thefella.com>
2012-03-10 21:59:58 +0000

    Added font detection script
    Visible symbol to show if the chosen font is available or not.

--- responsive-type.php
+++ responsive-type.php
@@ -79,7 +79,7 @@ span.font-name { display : block; font-size : 14px; color : #ccc; padding-bottom
 
 <body>
 <?php foreach ($fonts as $font) : ?>
-<article id="<?=strtolower($font)?>" style="font-family:'<?=$font?>';">
+<article id="<?=str_replace(" ", "", strtolower($font))?>" style="font-family:'<?=$font?>';">
 
 <h2><span class="font-name"><?=$font?></span> Type readability reference</h2>
 <?php foreach ($normal as $size) : ?>
@@ -98,5 +98,83 @@ span.font-name { display : block; font-size : 14px; color : #ccc; padding-bottom
 
 </article>
 <?php endforeach; ?>
+
+<?php if (($_GET['js']) != 'no') : ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>
+/**
+ * JavaScript code to detect available availability of a 
+ * particular font in a browser using JavaScript and CSS. 
+ * 
+ * Author : Lalit Patel
+ * Website: http://www.lalit.org/lab/jsoncookies
+ * License: Apache Software License 2.0
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ * Version: 0.15 (21 Sep 2009)
+ *          Changed comparision font to default from sans-default-default, 
+ *          as in FF3.0 font of child element didn't fallback 
+ *          to parent element if the font is missing.
+ * Version: 0.2 (04 Mar 2012)
+ *          Comparing font against all the 3 generic font families ie, 
+ *          'monospace', 'sans-serif' and 'sans'. If it doesn't match all 3 
+ *          then that font is 100% not available in the system
+ */
+
+/**
+ * Usage: d = new Detector();
+ *        d.detect('font name');
+ */
+var Detector = function(){
+	// a font will be compared against all the three default fonts.
+	// and if it doesn't match all 3 then that font is not available.
+	var baseFonts = ['monospace','sans-serif','sans'];	
+	
+	//we use m or w because these two characters take up the maximum width. 
+	// And we use a LLi so that the same matching fonts can get separated
+	var testString = "mmmmmmmmmmlli";
+	
+	//we test using 72px font size, we may use any size. I guess larger the better.
+	var testSize = '72px';
+	
+	var h = document.getElementsByTagName("body")[0];
+
+	// create a SPAN in the document to get the width of the text we use to test
+	var s = document.createElement("span");
+	s.style.fontSize   = testSize;			
+	s.innerHTML        = testString;
+	var defaultWidth   = {};
+	var defaultHeight  = {};
+	for(var index in baseFonts) {
+		//get the default width for the three base fonts
+		s.style.fontFamily = baseFonts[index];
+		h.appendChild(s);
+		defaultWidth[baseFonts[index]]   = s.offsetWidth;  //width for the default font
+		defaultHeight[baseFonts[index]]  = s.offsetHeight; //height for the defualt font
+		h.removeChild(s);
+	}
+
+	function detect(font) {
+		var detected = false;
+		for(var index in baseFonts) {
+			s.style.fontFamily = font + ',' + baseFonts[index];	// name of the font along with the base font for fallback.
+			h.appendChild(s);
+			var matched = (s.offsetWidth != defaultWidth[baseFonts[index]] || s.offsetHeight != defaultHeight[baseFonts[index]]);
+			h.removeChild(s);
+			detected = detected || matched;
+		}
+		return detected;
+	}
+	
+	this.detect = detect;	
+};
+var detective = new Detector();
+<?php foreach ($fonts as $font) : ?>
+var colour = (detective.detect('<?=$font?>') === true) ? '#6fbf4d' : '#e06666';
+var box = '<span style="display:inline-block; width:8px; height:8px; margin-right:6px; background-color:'+colour+';"></span>';
+var element = $("#<?=str_replace(" ", "", strtolower($font))?> h2 span");
+element.prepend(box);
+<?php endforeach; ?>
+</script>
+<?php endif; ?>
 </body>
 </html>
